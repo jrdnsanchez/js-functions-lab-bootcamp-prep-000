84a8e741f2ac4e1d127d0ddb0aea54eb9f4664e7
Jordan Sanchez <jrdnsanchez@gmail.com>
2019-08-30 03:12:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function happyHolidays() {
   return"Happy holidays!"
 }
-function happyHolidaysTo(name) {
-  return'Happy ${holidays}, ${name}!';
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
